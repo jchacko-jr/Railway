d355734531b5a23f0f7bdeb642bd0b8b273afab2
Jomon Chacko <54240334+jchacko-jr@users.noreply.github.com>
2023-10-30 13:49:54 +0530

    update

--- assets/inc/navbar.php
+++ assets/inc/navbar.php
@@ -12,12 +12,11 @@
     while($row=$res->fetch_object())
     {
 ?>
-    <nav class="navbar navbar-expand fixed-top be-top-header">
+     <nav class="navbar navbar-expand fixed-top be-top-header">
         <div class="container-fluid">
           <div class="be-navbar-header"><a class="navbar-brand" href="pass-dashboard.php"></a>
           </div>
           <div class="page-title"><span>
-          
           <?php 
           $welcome_string="Hello"; 
           $numeric_date=date("G");
