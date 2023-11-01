69c3211f4247635781739463465c43689cc3f915
Jomon Chacko <jomunchacko@gmail.com>
2023-11-01 22:31:40 +0530

    Create railway (1).sql

---
+++ db revised on 2023-1-11/railway (1).sql
@@ -0,0 +1,318 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2023 at 05:16 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `railway`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orrs_admin`
+--
+
+CREATE TABLE `orrs_admin` (
+  `admin_id` int(20) NOT NULL,
+  `admin_fname` varchar(200) NOT NULL,
+  `admin_lname` varchar(200) NOT NULL,
+  `admin_email` varchar(200) NOT NULL,
+  `admin_uname` varchar(200) NOT NULL,
+  `admin_pwd` varchar(200) NOT NULL,
+  `admin_dpic` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `orrs_admin`
+--
+
+INSERT INTO `orrs_admin` (`admin_id`, `admin_fname`, `admin_lname`, `admin_email`, `admin_uname`, `admin_pwd`, `admin_dpic`) VALUES
+(1, 'System ', 'Admin', 'admin@mail.com', 'Administrator', '4028a0e356acc947fcd2bfbf00cef11e128d484a', 'admin-icn.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orrs_contact`
+--
+
+CREATE TABLE `orrs_contact` (
+  `f_name` varchar(10) NOT NULL,
+  `l_name` varchar(10) NOT NULL,
+  `email` varchar(15) NOT NULL,
+  `number` int(10) NOT NULL,
+  `message` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orrs_contact`
+--
+
+INSERT INTO `orrs_contact` (`f_name`, `l_name`, `email`, `number`, `message`) VALUES
+('jomon', 'chacko', 'jomunchacko@gma', 2147483647, 'hey happpy to help'),
+('jomon', 'chacko', 'jomunchacko@gma', 2147483647, 'hey happpy to help'),
+('jomon', 'chacko', 'jomunchacko@gma', 2147483647, 'hey happpy to help'),
+('jomon', 'chacko', 'jomunchacko@gma', 2147483647, 'jfgfhdfdh'),
+('jomon', 'chacko', 'jomunchacko@gma', 2147483647, 'data is inserted'),
+('jomon', 'chacko', 'jomunchacko@gma', 2147483647, 'data is inserted'),
+('jomon', 'chacko', 'jochacko47@gmai', 2147483647, 'Test47'),
+('jomon', 'chacko', 'jomunchacko@gma', 2147483647, 'testonceisverfied');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orrs_employee`
+--
+
+CREATE TABLE `orrs_employee` (
+  `emp_id` int(20) NOT NULL,
+  `emp_fname` varchar(200) NOT NULL,
+  `emp_lname` varchar(200) NOT NULL,
+  `emp_nat_idno` varchar(200) NOT NULL,
+  `emp_phone` varchar(200) NOT NULL,
+  `emp_addr` varchar(200) NOT NULL,
+  `emp_uname` varchar(200) NOT NULL,
+  `emp_email` varchar(200) NOT NULL,
+  `emp_pwd` varchar(200) NOT NULL,
+  `emp_dpic` varchar(200) NOT NULL,
+  `emp_dept` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `orrs_employee`
+--
+
+INSERT INTO `orrs_employee` (`emp_id`, `emp_fname`, `emp_lname`, `emp_nat_idno`, `emp_phone`, `emp_addr`, `emp_uname`, `emp_email`, `emp_pwd`, `emp_dpic`, `emp_dept`) VALUES
+(4747, 'jomon', 'jomon', '745', '789456123', '123 street chry', 'jomon123', 'test123@gmail.com', '4028a0e356acc947fcd2bfbf00cef11e128d484a', 'New Project.png', '785');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orrs_passenger`
+--
+
+CREATE TABLE `orrs_passenger` (
+  `pass_id` int(20) NOT NULL,
+  `pass_fname` varchar(200) NOT NULL,
+  `pass_lname` varchar(200) NOT NULL,
+  `pass_phone` varchar(200) NOT NULL,
+  `pass_addr` varchar(200) NOT NULL,
+  `pass_email` varchar(200) NOT NULL,
+  `pass_pwd` varchar(200) NOT NULL,
+  `pass_dpic` varchar(200) NOT NULL,
+  `pass_uname` varchar(200) NOT NULL,
+  `pass_bday` varchar(200) NOT NULL,
+  `pass_bio` longtext NOT NULL,
+  `pass_train_number` varchar(200) NOT NULL,
+  `pass_train_name` varchar(200) NOT NULL,
+  `pass_dep_station` varchar(200) NOT NULL,
+  `pass_dep_time` varchar(200) NOT NULL,
+  `pass_arr_station` varchar(200) NOT NULL,
+  `pass_train_fare` varchar(200) NOT NULL,
+  `pass_fare_payment_code` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `orrs_passenger`
+--
+
+INSERT INTO `orrs_passenger` (`pass_id`, `pass_fname`, `pass_lname`, `pass_phone`, `pass_addr`, `pass_email`, `pass_pwd`, `pass_dpic`, `pass_uname`, `pass_bday`, `pass_bio`, `pass_train_number`, `pass_train_name`, `pass_dep_station`, `pass_dep_time`, `pass_arr_station`, `pass_train_fare`, `pass_fare_payment_code`) VALUES
+(7, 'Raisa', 'Taylor', '8542221450', '22 Valley Street', 'taylor@mail.com', '55c3b5386c486feb662a0785f340938f518d547f', 'defaultimg.jpg', 'raisataylor', '', '', '', '', '', '', '', '', ''),
+(8, 'Michael', 'Murdock', '7414587744', '43 Private Lane', 'murdock@mail.com', '55c3b5386c486feb662a0785f340938f518d547f', 'defaultimg.jpg', 'murdock', '', '', '', '', '', '', '', '', ''),
+(12, 'Martha', 'Meyer', '4570001569', '12 Eva Pearl Street', 'martha@mail.com', '55c3b5386c486feb662a0785f340938f518d547f', 'defaultimg.jpg', 'martha', '', '', '', '', '', '', '', '', ''),
+(16, 'Noelle', 'Ross', '3745698850', '41 Ray Court', 'ross@mail.com', '55c3b5386c486feb662a0785f340938f518d547f', 'defaultimg.jpg', 'noelle', '', '', '', '', '', '', '', '', ''),
+(19, 'Larry', 'Clark', '7696965450', '39 Dawson Drive', 'larry@mail.com', '55c3b5386c486feb662a0785f340938f518d547f', 'defaultimg.jpg', 'larry', '', '', '', '', '', '', '', '', ''),
+(20, 'Liam', 'Moore', '4750001458', '114 Southcross Avenue', 'liamoore@mail.com', '55c3b5386c486feb662a0785f340938f518d547f', 'defaultimg.jpg', 'liamoore', '', '', 'CA-778', 'Adirondack', 'Seattle', '6:00 AM', 'New York', '198', '100000789640'),
+(22, 'Jomon', '', '', '', '', '67a74306b06d0c01624fe0d0249a570f4d093747', '', '', '', '', '', '', '', '', '', '', ''),
+(23, 'Jomon', '', '', '', '', '67a74306b06d0c01624fe0d0249a570f4d093747', '', '', '', '', '', '', '', '', '', '', ''),
+(24, 'qqqq', 'wwww', 'wwww', 'wwww', 'sss@gamail.com', '268342a1a7c8dc64a8ad545c495ffe683ed473c8', '', 'www', '', '', '', '', '', '', '', '', ''),
+(29, 'jowe', 'eee', 'eee', 'eee', 'eee@gmail.com', '42d6d602e487078edc5756bc17d3a19ed176e0a1', '', 'eee', '', '', '', '', '', '', '', '', ''),
+(30, 'test', 'test', '789456123', 'puthentpurackal', 'test@gmail.com', '4028a0e356acc947fcd2bfbf00cef11e128d484a', 'New_Project-removebg-preview.png', 'jo47__', '', '', '', '', '', '', '', '', ''),
+(31, 'test12', 'test12', '78451245', 'testtts', 'test@47.in.com', '4028a0e356acc947fcd2bfbf00cef11e128d484a', '', 'test@47', '', '', '', '', '', '', '', '', ''),
+(32, 'server', 'test', '741258963', 'server', 'test@server.com', '4028a0e356acc947fcd2bfbf00cef11e128d484a', 'favicon.ico', 'test@server', '', '', '16305', 'Venad Express', 'TRIVANDRUM', '5.00AM', 'ERNAKULAM', '60', 'INR');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orrs_passwordresets`
+--
+
+CREATE TABLE `orrs_passwordresets` (
+  `pwd_id` int(20) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `status` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `orrs_passwordresets`
+--
+
+INSERT INTO `orrs_passwordresets` (`pwd_id`, `email`, `status`) VALUES
+(1, 'employee@mail.com', 'Approved'),
+(2, 'test21@mail.com', ''),
+(3, 'test@in.com', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orrs_train`
+--
+
+CREATE TABLE `orrs_train` (
+  `id` int(20) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `route` varchar(200) NOT NULL,
+  `current` varchar(200) NOT NULL,
+  `destination` varchar(200) NOT NULL,
+  `time` varchar(200) NOT NULL,
+  `passengers` varchar(200) NOT NULL,
+  `number` varchar(200) NOT NULL,
+  `fare` varchar(2000) NOT NULL,
+  `fare_payment_code` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `orrs_train`
+--
+
+INSERT INTO `orrs_train` (`id`, `name`, `route`, `current`, `destination`, `time`, `passengers`, `number`, `fare`, `fare_payment_code`) VALUES
+(6, 'Venad Express', 'TRIVANDRUM _ERNAKULAM', 'TRIVANDRUM', 'ERNAKULAM', '5.00AM', '200', '16305', '60', NULL),
+(10, 'ZX Express', 'Location One - Location Two', 'Test Station', 'Test Demo Station', '7:00 PM', '200', 'CA556680', '65', NULL),
+(11, 'Iron Range Express', 'Stockton - San Diego', 'Stockton', 'San Diego', '9:00 AM', '185', 'CA-697', '43', NULL),
+(12, 'Colonial', 'Boston - New York', 'Boston', 'New York', '10:45 AM', '255', 'CA-107', '35', NULL),
+(13, 'Corridor Express', 'New York - Washington', 'New York', 'Washington', '1:00 PM', '330', 'CA-777', '38', NULL),
+(14, 'Silver Meteor', 'New York - Miami', 'New York', 'Miami', '10:00 AM', '200', 'CA-707', '128', NULL),
+(15, 'Silver Star', 'New Orleans - Los Angeles', 'New Orleans', 'Los Angeles', '8:45 AM', '190', 'CA-107', '149', NULL),
+(16, 'Adirondack', 'Seattle - New York', 'Seattle', 'New York', '6:00 AM', '210', 'CA-778', '198', NULL),
+(17, 'Corridor Express', 'New York - Pittsfield', 'New York', 'Pittsfield', '12:45 PM', '195', 'CA-797', '45', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orrs_train_tickets`
+--
+
+CREATE TABLE `orrs_train_tickets` (
+  `ticket_id` int(20) NOT NULL,
+  `pass_name` varchar(200) NOT NULL,
+  `pass_email` varchar(200) NOT NULL,
+  `pass_addr` varchar(200) NOT NULL,
+  `train_name` varchar(200) NOT NULL,
+  `train_no` varchar(200) NOT NULL,
+  `train_dep_stat` varchar(200) NOT NULL,
+  `train_arr_stat` varchar(200) NOT NULL,
+  `train_fare` varchar(200) NOT NULL,
+  `fare_payment_code` varchar(200) NOT NULL,
+  `confirmation` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `orrs_train_tickets`
+--
+
+INSERT INTO `orrs_train_tickets` (`ticket_id`, `pass_name`, `pass_email`, `pass_addr`, `train_name`, `train_no`, `train_dep_stat`, `train_arr_stat`, `train_fare`, `fare_payment_code`, `confirmation`) VALUES
+(5, 'Christine Moore', 'christine@mail.com', '44 Demo Address', 'ZX Express', 'CA556680', 'Test Station', 'Test Demo Station', '65', 'CAS0014587', ''),
+(6, 'John Barnes', 'johnk@mail.com', '32 Ocello Street', 'Iron Range Express', 'CA-697', 'Stockton', 'San Diego', '43', '102458700041', 'Approved'),
+(7, 'John Barnes', 'johnk@mail.com', '32 Ocello Street', 'Corridor Express', 'CA-777', 'New York', 'Washington', '38', '102458700041', 'Approved'),
+(8, 'Fritz Mickk', 'fritz@mail.com', '80 Russell Street', 'Black Water', 'CA-007', 'Chicago', 'Carbondale', '33', '107856452120', 'Approved'),
+(9, 'Liam Moore', 'liamoore@mail.com', '114 Southcross Avenue', 'Adirondack', 'CA-778', 'Seattle', 'New York', '198', '100000789640', 'Approved'),
+(10, 'server test', 'test@server.com', 'server', 'Venad Express', '16305', 'TRIVANDRUM', 'ERNAKULAM', '60', 'INR', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orrs_admin`
+--
+ALTER TABLE `orrs_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `orrs_employee`
+--
+ALTER TABLE `orrs_employee`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- Indexes for table `orrs_passenger`
+--
+ALTER TABLE `orrs_passenger`
+  ADD PRIMARY KEY (`pass_id`);
+
+--
+-- Indexes for table `orrs_passwordresets`
+--
+ALTER TABLE `orrs_passwordresets`
+  ADD PRIMARY KEY (`pwd_id`);
+
+--
+-- Indexes for table `orrs_train`
+--
+ALTER TABLE `orrs_train`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orrs_train_tickets`
+--
+ALTER TABLE `orrs_train_tickets`
+  ADD PRIMARY KEY (`ticket_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orrs_admin`
+--
+ALTER TABLE `orrs_admin`
+  MODIFY `admin_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orrs_employee`
+--
+ALTER TABLE `orrs_employee`
+  MODIFY `emp_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4748;
+
+--
+-- AUTO_INCREMENT for table `orrs_passenger`
+--
+ALTER TABLE `orrs_passenger`
+  MODIFY `pass_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `orrs_passwordresets`
+--
+ALTER TABLE `orrs_passwordresets`
+  MODIFY `pwd_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `orrs_train`
+--
+ALTER TABLE `orrs_train`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `orrs_train_tickets`
+--
+ALTER TABLE `orrs_train_tickets`
+  MODIFY `ticket_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
